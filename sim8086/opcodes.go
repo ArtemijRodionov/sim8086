299607743bbb223538f320b80fe6511311b9bc06
Artemij Rodionov <wertins71@gmail.com>
2023-09-05 16:36:20 +0300

    register encoding wip

--- sim8086/opcodes.go
+++ sim8086/opcodes.go
@@ -1,50 +1,75 @@
 package sim
 
 type First byte
+type Opcode byte
 type D byte
 type W byte
+
+const (
+	directionSrc D = 0x0
+	directionDst D = 0x1
+)
+
+const (
+	operationByte W = 0x0
+	operationWord W = 0x1
+)
+
+func (o First) W() W {
+	switch w := W(o & 1); w {
+	case operationByte, operationWord:
+		return w
+	default:
+		panic("can't read W")
+	}
+}
+
+func (o First) D() D {
+	switch d := D(o & (1 << 1)); d {
+	case directionSrc, directionDst:
+		return d
+	default:
+		panic("can't parse D")
+	}
+}
 
 type Second byte
 type Mod byte
 type Reg byte
-type RM byte
-
-type Instruction struct {
-	First
-	Second
-}
 
 const (
-	byteOperation W = 0x0
-	wordOperation W = 0x1
+	modMemOffset0  Mod = 0x0 << 6
+	modMemOffset8  Mod = 0x1 << 6
+	modMemOffset16 Mod = 0x2 << 6
+	modRegOffset0  Mod = 0x3 << 6
 )
 
 const (
-	MemoryNoDisplacement   Mod = 0x0 << 6
-	MemoryDisplacement8    Mod = 0x1 << 6
-	MemoryDisplacement16   Mod = 0x2 << 6
-	RegisterNoDisplacement Mod = 0x3 << 6
+	alax = 0x0 << 3
+	clcx = 0x1 << 3
+	dldx = 0x2 << 3
+	blbx = 0x3 << 3
+	ahsp = 0x4 << 3
+	chbp = 0x5 << 3
+	dhsi = 0x6 << 3
+	bhdi = 0x7 << 3
 )
 
-func (o First) W() W {
-	switch new := W(o & 1); new {
-	case byteOperation, wordOperation:
-		return new
+func (o Second) Mod() Mod {
+	switch d := Mod(o & 0xe0); d {
+	case modMemOffset0, modMemOffset8, modMemOffset16, modRegOffset0:
+		return d
 	default:
-		panic("can't read D")
+		panic("can't parse Mod")
 	}
 }
-func (o First) D() D {
-	return D(o & (1 << 1))
-}
-func (o Second) Mod() Mod {
-	return Mod(0)
-}
+
 func (o Second) Reg() Reg {
 	return Reg(0)
 }
-func (o Second) RM() RM {
-	return RM(0)
+
+func (o Second) RM() Reg {
+	return Reg(0)
 }
 
 type regEncoding struct {
@@ -53,22 +78,22 @@ type regEncoding struct {
 }
 
 var registerEncoding = map[regEncoding]string{
-	{0x0 << 3, byteOperation}: "al",
-	{0x1 << 3, byteOperation}: "cl",
-	{0x2 << 3, byteOperation}: "dl",
-	{0x3 << 3, byteOperation}: "bl",
-	{0x4 << 3, byteOperation}: "ah",
-	{0x5 << 3, byteOperation}: "ch",
-	{0x6 << 3, byteOperation}: "dh",
-	{0x7 << 3, byteOperation}: "bh",
-	{0x0 << 3, wordOperation}: "ax",
-	{0x1 << 3, wordOperation}: "cx",
-	{0x2 << 3, wordOperation}: "dx",
-	{0x3 << 3, wordOperation}: "bx",
-	{0x4 << 3, wordOperation}: "sp",
-	{0x5 << 3, wordOperation}: "bp",
-	{0x6 << 3, wordOperation}: "si",
-	{0x7 << 3, wordOperation}: "di",
+	{alax, operationByte}: "al",
+	{clcx, operationByte}: "cl",
+	{dldx, operationByte}: "dl",
+	{blbx, operationByte}: "bl",
+	{ahsp, operationByte}: "ah",
+	{chbp, operationByte}: "ch",
+	{dhsi, operationByte}: "dh",
+	{bhdi, operationByte}: "bh",
+	{alax, operationWord}: "ax",
+	{clcx, operationWord}: "cx",
+	{dldx, operationWord}: "dx",
+	{blbx, operationWord}: "bx",
+	{ahsp, operationWord}: "sp",
+	{chbp, operationWord}: "bp",
+	{dhsi, operationWord}: "si",
+	{bhdi, operationWord}: "di",
 }
 
 func (r regEncoding) String() string {
@@ -77,4 +102,9 @@ func (r regEncoding) String() string {
 		panic("Can't encode register")
 	}
 	return val
+}
+
+type Instruction struct {
+	First
+	Second
 }
